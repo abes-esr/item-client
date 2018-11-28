2d7e4964ffe9caa32e7ff105220556c92db66b00
Théo Chambon <chambon@abes.fr>
2018-11-28 09:50:08 +0100

    MAJ script build pour déploiement puppet

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -25,7 +25,7 @@ module.exports = {
     app: './src/main.js'
   },
   output: {
-    path: config.build.assetsRoot,
+    path: config.build.assetsRoot + "-" + process.env.confEnv,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -23,7 +23,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   },
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
-    path: config.build.assetsRoot,
+    path: config.build.assetsRoot + "-" + process.env.confEnv,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
