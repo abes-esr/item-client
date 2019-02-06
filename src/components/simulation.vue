187d7e58fc0f0f1e273642f230c3beb4befe87ad
Théo Chambon <chambon@abes.fr>
2019-02-06 10:23:31 +0100

    #110 Fix : Redirection sur le tableau de bord après le lancement du traitement
    La redirection était mal orthographiée et ne fonctionnait pas, ça devrait être ok maintenant.

--- src/components/simulation.vue
+++ src/components/simulation.vue
@@ -24,7 +24,7 @@
             <v-divider></v-divider>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="primary" flat @click="dialog = false, $router.push({ name: 'tableau' })">OK</v-btn>
+              <v-btn color="primary" flat @click="dialog = false, $router.push({ name: 'tab' })">OK</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
