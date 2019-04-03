139e8aefa8c6d2830a7fde7744ca00027609a1c0
Théo Chambon <chambon@abes.fr>
2019-04-03 11:50:14 +0200

    #186 Feat : Dans le fil d'Ariane, Simulation est remplacé par Production

--- src/components/utils/stepper.vue
+++ src/components/utils/stepper.vue
@@ -10,7 +10,7 @@
       <v-divider></v-divider>
       <v-stepper-step step="4" :complete="current > 4">Fichier complété</v-stepper-step>
       <v-divider></v-divider>
-      <v-stepper-step step="5" :complete="current > 5">Simulation</v-stepper-step>
+      <v-stepper-step step="5" :complete="current > 5">Production</v-stepper-step>
     </v-stepper-header>
   </v-stepper>
 </template>
