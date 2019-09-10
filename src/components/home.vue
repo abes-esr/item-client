26ab4646f65d0b6d86a971efb22328e2ecf797c7
jerome <jvk@abes.fr>
2019-09-10 16:23:54 +0200

    integration nouveau systeme de grille menu

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,15 @@
 <template>
   <!-- PAGE D'ACCUEIL -->
-  <v-row  class="container">
+  <v-row class="container" style="margin-top: 5em;">
     <v-col class="fill-height ma-2 pt-5" cols="12" md="5" >
-      <menu-cards titleName="Créer des exemplaires" iconType="add_circle_outline" routerLink="rcrEx" :cardHeight=7.8></menu-cards>
-      <menu-cards titleName="Supprimer des exemplaires" iconType="delete" routerLink="tab" :cardHeight=7.8></menu-cards>
-      <menu-cards titleName="Gérer mes demandes d'exemplarisation" iconType="sort" routerLink="tabEx" :cardHeight=7.8></menu-cards>
+      <menu-cards titleName="Créer des <br/>exemplaires" iconType="add_circle_outline" routerLink="rcrEx" :cardHeight=7.8></menu-cards>
+      <menu-cards titleName="Supprimer des <br/>exemplaires" iconType="delete" routerLink="tab" :cardHeight=7.8></menu-cards>
+      <menu-cards titleName="Gérer mes demandes <br/> d'exemplarisation" iconType="sort" routerLink="tabEx" :cardHeight=7.8></menu-cards>
     </v-col>
     <v-col class="fill-height ma-2 pt-5" cols="12" md="5" >
-      <menu-cards titleName="Modifier des exemplaires" iconType="edit" routerLink="rcr" :cardHeight=7.8></menu-cards>
-      <menu-cards titleName="Calculer mon taux de recouvrement" iconType="pie_chart" routerLink="tab" :cardHeight=7.8></menu-cards>
-      <menu-cards titleName="Gérer mes demandes de modification" iconType="sort" routerLink="tab" :cardHeight=7.8></menu-cards>
+      <menu-cards titleName="Modifier des <br/>exemplaires" iconType="edit" routerLink="rcr" :cardHeight=7.8></menu-cards>
+      <menu-cards titleName="Calculer mon taux <br/>de recouvrement" iconType="pie_chart" routerLink="tab" :cardHeight=7.8></menu-cards>
+      <menu-cards titleName="Gérer mes demandes <br/>de modification" iconType="sort" routerLink="tab" :cardHeight=7.8></menu-cards>
     </v-col>
   </v-row>
 </template>
--- src/components/utils/menuCards.vue
+++ src/components/utils/menuCards.vue
@@ -1,16 +1,16 @@
 <template>
   <!-- Card de menu cliquable réutilisable -->
-  <v-card raised hover @click="route()" :height="cardHeight + 'em'">
+  <v-card hover @click="route()" :height="cardHeight + 'em'">
     <v-card-title class="primary-title">
       <v-container class="grid-list-md">
         <v-row class="row wrap align-center">
-          <v-col class="xs2">
-            <span class="group pa-3 secondary" :style="{'height': cardHeight-3.6 + 'em'}">
-              <v-icon style="margin-top: -7px" x-large dark color="white">{{ iconType }}</v-icon>
+          <v-col v-for="n in 1" :key="n" cols="12" sm="3">
+            <span class="group">
+              <v-icon x-large >{{ iconType }}</v-icon>
             </span>
           </v-col>
-          <v-col class="xs9 ml-3">
-            <h3 class="headline">{{ titleName }}</h3>
+          <v-col v-for="n in 1" :key="n" cols="12" sm="9">
+            <h3 v-html="titleName"></h3>
           </v-col>
         </v-row>
       </v-container>
