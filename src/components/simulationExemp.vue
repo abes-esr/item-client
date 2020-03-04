7589c0eca3d28a992bb4ff394b28ab83ccf29307
jerome <jvk@abes.fr>
2020-03-04 15:30:28 +0100

    #480 Bug: remove conditionnaly disabled button for launch treatment

--- src/components/simulationExemp.vue
+++ src/components/simulationExemp.vue
@@ -217,11 +217,8 @@
           <v-btn large color="info" @click="etapeDemande(numDem, modif, 3)" aria-label="Annuler" style="margin-right: 1em">
             Précédent
           </v-btn>
-          <v-btn v-if="!alert" large color="info" @click="dialog = true"
+          <v-btn large color="info" @click="dialog = true"
                  aria-label="Lancer le traitement en production">Lancer le traitement en production
-          </v-btn>
-          <v-btn v-if="alert" large color="info"
-                 aria-label="Lancer le traitement en production" disabled>Lancer le traitement en production
           </v-btn>
         </v-row>
       </v-col>
