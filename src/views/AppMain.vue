298e5ec385cf327e35dc87c4dfccbe470ebac330
SamuelQuetin <samquetinuel@gmail.com>
2024-06-17 10:56:45 +0200

    refactor v-card useless

--- src/views/AppMain.vue
+++ src/views/AppMain.vue
@@ -1,108 +1,43 @@
 <template>
-	<v-container class="fill-height">
-		<v-responsive class="align-centerfill-height mx-auto">
-			<div class="py-4" />
-			<v-row>
-        <v-col cols="12" md="6" class="text-center">
-          <v-card
-            class="py-4"
-            color="surface-variant"
-            @click="console.log('router.push(Calcul du taux de recouvrement)')"
-            prepend-icon="mdi-percent"
-            rel="noopener noreferrer"
-            rounded="lg"
-            subtitle="Calcul du taux de recouvrement"
-            target="_blank"
-            title="Recouvrement"
-            variant="text"
-          >
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
+  <v-container class="fill-height">
+    <v-responsive class="align-centerfill-height mx-auto">
+      <div class="py-5"/>
+      <v-row>
+        <v-col class="text-center" cols="12" md="6">
+          <bouton-item icon="mdi-percent" route="tauxRecouv">
+            Calculer mon taux de recouvrement
+          </bouton-item>
         </v-col>
-        <v-col cols="12" md="6" class="text-center">
-          <v-card
-            class="py-4"
-            color="surface-variant"
-            @click="router.push('recouvrementTableau')"
-            prepend-icon="mdi-chart-bar"
-            rel="noopener noreferrer"
-            rounded="lg"
-            subtitle="Gérer ses taux de recouvrement"
-            title="Recouvrement"
-            variant="text"
-          >
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
+        <v-col class="text-center" cols="12" md="6">
+          <bouton-item icon="mdi-chart-bar" route="recouvrementTableau">
+            Gérer mes taux de recouvrement
+          </bouton-item>
         </v-col>
-				<v-col cols="12" md="6" class="text-center">
-					<v-card
-						class="py-4"
-						color="surface-variant"
-						@click="console.log('router.push(Créer de nouveaux exemplaires)')"
-						prepend-icon="mdi-plus"
-						rel="noopener noreferrer"
-						rounded="lg"
-						subtitle="Créer de nouveaux exemplaires"
-						target="_blank"
-						title="Exemplarisation"
-						variant="text"
-					>
-						<v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-					</v-card>
-				</v-col>
-				<v-col cols="12" md="6" class="text-center">
-					<v-card
-						class="py-4"
-						color="surface-variant"
-            @click="router.push('exemplarisationTableau')"
-						prepend-icon="mdi-chart-bar"
-						rel="noopener noreferrer"
-						rounded="lg"
-						subtitle="Gérer ses exemplarisations"
-						title="Exemplarisation"
-						variant="text"
-					>
-						<v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-					</v-card>
-				</v-col>
-
-				<v-col cols="12" md="6" class="text-center">
-					<v-card
-						class="py-4"
-						color="surface-variant"
-            @click="console.log('router.push(Modifier des exemplaires)')"
-						prepend-icon="mdi-pencil"
-						rel="noopener noreferrer"
-						rounded="lg"
-						subtitle="Modifier des exemplaires"
-						title="Modification"
-						variant="text"
-					>
-						<v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-					</v-card>
-				</v-col>
-        <v-col cols="12" md="6" class="text-center">
-          <v-card
-            class="py-4"
-            color="surface-variant"
-            @click="router.push('modificationTableau')"
-            prepend-icon="mdi-chart-bar"
-            rel="noopener noreferrer"
-            rounded="lg"
-            subtitle="Gérer ses modifications"
-            title="Modification"
-            variant="text"
-          >
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
+        <v-col class="text-center" cols="12" md="6">
+          <bouton-item icon="mdi-plus" route="nouveauxExemplaires">
+            Créer des exemplaires
+          </bouton-item>
         </v-col>
-			</v-row>
-		</v-responsive>
-	</v-container>
+        <v-col class="text-center" cols="12" md="6">
+          <bouton-item icon="mdi-chart-bar" route="exemplarisationTableau">
+            Gérer mes créations
+          </bouton-item>
+        </v-col>
+        <v-col class="text-center" cols="12" md="6">
+          <bouton-item icon="mdi-pencil" route="modifExempl">
+            Modifier des exemplaires
+          </bouton-item>
+        </v-col>
+        <v-col class="text-center" cols="12" md="6">
+          <bouton-item icon="mdi-chart-bar" route="modificationTableau">
+            Gérer mes modifications
+          </bouton-item>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
 </template>
 
 <script setup>
-import router from "@/router"
-
-//
+import BoutonItem from '@/components/Home/BoutonItem.vue';
 </script>
