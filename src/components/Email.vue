71f8e0ee5ac64af5a40b0552d821012b22edf748
SamuelQuetin <quetin@abes.fr>
2024-06-20 10:34:54 +0200

    Peut pas valider si respecte pas regle

--- src/components/Email.vue
+++ src/components/Email.vue
@@ -1,15 +1,17 @@
 <template>
-  <v-card class="elevation-12">
+  <v-card class="elevation-5">
     <v-card-title>
-      <span v-if="!email">Première connexion</span>
+      <span class="font-weight-black" v-if="!email">Première connexion</span>
       <span v-else>Modifier son Email</span>
     </v-card-title>
     <v-card-text>
-      <v-form ref="form" lazy-validation>
+      <v-form ref="form">
         <span>
           Votre adresse mail est obligatoire pour utiliser l'application. Pour ajouter plusieurs adresses mail, séparez-les par des points virgules ;
         </span>
         <v-text-field
+          prepend-inner-icon="mdi-email-outline"
+          class="pt-12"
           type="email"
           name="email"
           id="email"
@@ -21,12 +23,16 @@
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="info" :disabled="isDisabled" @click="validate()">Valider</v-btn>
+      <v-btn
+        color="info"
+        variant="flat"
+        :disabled="isDisabled"
+        @click="validate()">Valider</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 <script setup>
-import { onMounted, ref, watch } from 'vue';
+import {onMounted, ref, watch} from 'vue';
 import demandesService from '@/service/DemandesService';
 
 const emits = defineEmits(['validate']);
@@ -46,11 +52,19 @@ const isDisabled = ref(true);
 const pattern = /^((([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))[;]?)+$/;
 const rules = ref([
   value => {
-    if (value) return true;
+    if (value) {
+      isDisabled.value = false;
+      return true;
+    }
+    isDisabled.value = true;
     return 'Champ obligatoire.';
   },
   value => {
-    if (pattern.test(value)) return true;
+    if (pattern.test(value)) {
+      isDisabled.value = false;
+      return true;
+    }
+    isDisabled.value = true;
     return 'mail(s) invalide'
   }
 ]);
@@ -65,8 +79,4 @@ function validate() {
       emits('validate', result.data.email);
     });
 }
-
-watch(emailModel.value, (newValue) => {
-  isDisabled.value = !rules.value.every(rule => rule(newValue)); // Check all rules
-});
 </script>
