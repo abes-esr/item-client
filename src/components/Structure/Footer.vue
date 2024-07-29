634078fc40fd688b9fc5a06d57e15105562827dd
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-07-29 15:00:31 +0200

    - Correction des v-tooltips

--- src/components/Structure/Footer.vue
+++ src/components/Structure/Footer.vue
@@ -1,14 +1,22 @@
 <template>
-  <v-footer app style="background-color: #295494" height="40">
+  <v-footer style="background-color: white" height="40">
     <div class="d-flex flex-wrap align-center">
-      <div class="d-flex flex-wrap align-center">
-        <v-chip class="ma-2" :color="chipColorCbsBaseStatus" variant="text"><v-icon icon="mdi-server" start></v-icon>CBS</v-chip>
-        <v-chip class="ma-2" :color="chipColorItemBaseStatus" variant="text"><v-icon icon="mdi-server" start></v-icon>XML</v-chip>
-        <v-chip class="ma-2" :color="chipColorXmlBaseStatus" variant="text"><v-icon icon="mdi-server" start></v-icon>ITEM</v-chip>
-      </div>
-
       <div class="ml-auto d-flex flex-wrap align-center text-caption">
-        <a class="ma-2 text-disabled" href="http://item-dev.sudoc.fr">Détails de l'application</a>
+        <v-tooltip location="top">
+          <template v-slot:activator="{ props }">
+            <a class="ma-2 text-disabled" v-bind="props">Etat des serveurs</a>
+          </template>
+              <v-chip class="ma-1" :color="chipColorCbsBaseStatus" variant="text"><v-icon icon="mdi-server" start></v-icon>CBS</v-chip>
+              <v-chip class="ma-1" :color="chipColorItemBaseStatus" variant="text"><v-icon icon="mdi-server" start></v-icon>XML</v-chip>
+              <v-chip class="ma-1" :color="chipColorXmlBaseStatus" variant="text"><v-icon icon="mdi-server" start></v-icon>ITEM</v-chip>
+        </v-tooltip>
+        <v-tooltip location="top">
+          <template v-slot:activator="{ props }">
+            <a class="ma-2 text-disabled" v-bind="props">Détail de l'application</a>
+          </template>
+              <v-chip class="ma-1" variant="text">Interface Version : {{}}</v-chip>
+              <v-chip class="ma-1" variant="text">Serveur Version</v-chip>
+        </v-tooltip>
         <a class="ma-2 text-disabled" href="http://item-dev.sudoc.fr">Données personnelles</a>
         <a class="ma-2 text-disabled" href="https://abes.fr/pages-cgu/conditions-generales-utilisation-sites-abes.html">Conditions générales d'utilisation (CGU)</a>
         <a class="ma-2 text-disabled" href="http://item-dev.sudoc.fr">Mentions légales</a>
@@ -22,8 +30,12 @@
 <script setup>
 import { ref, computed } from 'vue'
 
+
 const chipColorCbsBaseStatus = ref('green')
 const chipColorItemBaseStatus = ref('green')
 const chipColorXmlBaseStatus = ref('red')
 const currentYear = computed(() => new Date().getFullYear())
+
+
+
 </script>
