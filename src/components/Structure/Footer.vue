258d5d3e2c7e617d55124b70762a9b268193c59c
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-08-01 15:41:59 +0200

    - Finalisation des corrections relatives au problème de disposition du footer

--- src/components/Structure/Footer.vue
+++ src/components/Structure/Footer.vue
@@ -1,27 +1,29 @@
 <template>
-  <v-footer style="background-color: white" height="40">
-    <div class="d-flex flex-wrap align-center">
-      <div class="ml-auto d-flex flex-wrap align-center text-caption">
+  <v-footer style="background-color: #252c61; color: white; bottom: 0px; left: 0px; width: 100%; height: auto" class="mt-auto py-0">
+    <div class="d-flex flex-wrap justify-center align-center mt-0 mb-0" style="width: 100%">
+      <div class="d-flex flex-wrap justify-center align-center text-body-2">
         <v-tooltip location="top">
           <template v-slot:activator="{ props }">
-            <a class="ma-2 text-disabled" v-bind="props" @mouseenter="getHealthOfServices">Etat des serveurs</a>
+            <a class="ma-2 text-white" v-bind="props" @mouseenter="getHealthOfServices" style="cursor: pointer;">Etat des serveurs</a>
           </template>
-              <v-chip class="ma-1" :color="healthServices['STATUT CBS'] ? 'green' : 'red'" variant="text"><v-icon icon="mdi-server" start></v-icon>CBS</v-chip>
-              <v-chip class="ma-1" :color="healthServices['STATUT BASE_XML'] ? 'green' : 'red'" variant="text"><v-icon icon="mdi-server" start></v-icon>XML</v-chip>
-              <v-chip class="ma-1" :color="healthServices['STATUT BASE_ITEM'] ? 'green' : 'red'" variant="text"><v-icon icon="mdi-server" start></v-icon>ITEM</v-chip>
+          <v-chip class="ma-1" :color="healthServices['STATUT CBS'] ? 'green' : 'red'" variant="text"><v-icon icon="mdi-server" start></v-icon>CBS</v-chip>
+          <v-chip class="ma-1" :color="healthServices['STATUT BASE_XML'] ? 'green' : 'red'" variant="text"><v-icon icon="mdi-server" start></v-icon>XML</v-chip>
+          <v-chip class="ma-1" :color="healthServices['STATUT BASE_ITEM'] ? 'green' : 'red'" variant="text"><v-icon icon="mdi-server" start></v-icon>ITEM</v-chip>
         </v-tooltip>
         <v-tooltip location="top">
           <template v-slot:activator="{ props }">
-            <a class="ma-2 text-disabled" v-bind="props">Détail de l'application</a>
+            <a class="ma-2 text-white" v-bind="props" style="cursor: pointer;">Détail de l'application</a>
           </template>
-              <v-chip class="ma-1" variant="text">Interface Version : {{ packageVersion }}</v-chip>
-              <v-chip class="ma-1" variant="text">Serveur Version : {{ backVersion }}</v-chip>
+          <v-chip class="ma-1 text-white" variant="text">Interface Version : {{ packageVersion }}</v-chip>
+          <v-chip class="ma-1 text-white" variant="text">Serveur Version : {{ backVersion }}</v-chip>
         </v-tooltip>
-        <a class="ma-2 text-disabled" href="http://item-dev.sudoc.fr">Données personnelles</a>
-        <a class="ma-2 text-disabled" href="https://abes.fr/pages-cgu/conditions-generales-utilisation-sites-abes.html">Conditions générales d'utilisation (CGU)</a>
-        <a class="ma-2 text-disabled" href="http://item-dev.sudoc.fr">Mentions légales</a>
-        <a class="ma-2 text-disabled" href="https://abes.fr/pages-accessibilite/item.sudoc.html">Accessibilité Numérique</a>
-        <span class="ma-2 d-none d-sm-inline-block">{{ currentYear }} - ABES</span>
+        <a class="ma-2 text-white" href="http://item-dev.sudoc.fr">Données personnelles</a>
+        <a class="ma-2 text-white" href="https://abes.fr/pages-cgu/conditions-generales-utilisation-sites-abes.html">Conditions générales d'utilisation (CGU)</a>
+        <a class="ma-2 text-white" href="http://item-dev.sudoc.fr">Mentions légales</a>
+        <a class="ma-2 text-white" href="https://abes.fr/pages-accessibilite/item.sudoc.html">Accessibilité Numérique</a>
+        <div style="flex-grow: 5">
+          <span class="ma-2 d-none d-sm-inline-block text-white">{{ currentYear }} - ABES</span>
+        </div>
       </div>
     </div>
   </v-footer>
