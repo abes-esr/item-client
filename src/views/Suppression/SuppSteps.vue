79d34c329527961253a2c29c5b94eda40fcdbf5c
SamuelQuetin <quetin@abes.fr>
2024-09-17 15:49:04 +0200

    FEAT ajout de props dans le dialog + utilisation pour supp

--- src/views/Suppression/SuppSteps.vue
+++ src/views/Suppression/SuppSteps.vue
@@ -131,7 +131,7 @@
       </v-col>
     </v-row>
   </v-container>
-  <dialog-lancer-traitement v-model="dialog" :is-loading="isLoading" rubrique="Gérer mes suppressions" route="/suppression-tableau" @launch="launchDemande()"></dialog-lancer-traitement>
+  <dialog-lancer-traitement v-model="dialog" :is-loading="isLoading" rubrique="Gérer mes suppressions" route="/suppression-tableau" body="Le traitement de suppression  des exemplaires en base de production est irréversible." @launch="launchDemande()"></dialog-lancer-traitement>
   <dialog-suppression v-model="suppDialog" :demande="demande" return-to-accueil></dialog-suppression>
 </template>
 
