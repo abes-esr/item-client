90365da3ea9061773ccd9348954b36de5b56cc86
SamuelQuetin <samquetinuel@gmail.com>
2024-11-27 11:20:33 +0100

    ITEM-392 FEAT Ajout Composant Maintenance avec variable dans .env-dist

---
+++ src/components/Structure/Maintenance.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-overlay
+      absolute
+      v-model="warningDialog"
+      persistent
+      class="justify-center align-center"
+  >
+    <v-alert
+        type="warning"
+        prominent
+        border="left"
+    >
+      Service indisponible. Application en maintenance. Vous serez informé de la reprise du service.
+    </v-alert>
+  </v-overlay>
+</template>
+<script setup>
+
+
+import { computed } from 'vue';
+
+const warningDialog = computed(() => import.meta.env.VITE_API_MAINTENANCE === 'true');
+
+</script>
+<style>
+v-alert {
+  padding-bottom: 0 !important;
+  margin-bottom: 0 !important;
+}
+</style>
