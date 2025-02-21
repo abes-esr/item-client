54b8c5f028e1548f64f45572561a606a07ace758
SamuelQuetin <samquetinuel@gmail.com>
2025-02-21 10:19:57 +0100

    ITEM-511 FIX supprimer en supprimer cette demande

--- src/components/Modif/TypeTraitement.vue
+++ src/components/Modif/TypeTraitement.vue
@@ -2,10 +2,10 @@
   <v-card flat :loading="isLoading">
     <v-card-title class="d-flex justify-space-between custom-card-title">
       <span>Choix du type de traitement</span>
-      <v-btn depressed variant="text" @click="deleted()" prepend-icon="mdi-delete">Supprimer</v-btn>
+      <v-btn depressed variant="text" @click="deleted()" prepend-icon="mdi-delete">Supprimer cette demande</v-btn>
     </v-card-title>
 
-    <v-card-text  class="pa-0 ma-0">
+    <v-card-text class="pa-0 ma-0">
       <v-overlay v-if="isLoading" v-model="overlay" class="justify-center align-center" contained>
         <v-progress-circular
           color="info"
@@ -13,12 +13,13 @@
         ></v-progress-circular>
       </v-overlay>
       <v-hover v-slot="{ isHovering, props }" v-for="traitement in listTraitement" :key="traitement.id">
-        <div v-bind="props" :class="`btn-perso elevation-${isHovering ? 6 : 2} pa-5 ma-1 d-flex justify-space-between`" @click="onClick(traitement)">
+        <div v-bind="props" :class="`btn-perso elevation-${isHovering ? 6 : 2} pa-5 ma-1 d-flex justify-space-between`"
+             @click="onClick(traitement)">
           <v-row class="align-center">
             <v-col cols="12" sm="3" class="d-flex justify-center">
               <v-icon size="xx-large" dark color="primary">{{ getIcon(traitement) }}</v-icon>
             </v-col>
-            <v-col cols="12" sm="9" class="d-flex justify-start"><p class="group" >{{ traitement.libelle }}</p></v-col>
+            <v-col cols="12" sm="9" class="d-flex justify-start"><p class="group">{{ traitement.libelle }}</p></v-col>
           </v-row>
         </div>
       </v-hover>
