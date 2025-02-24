549dbd2c72cd7897a59e0a942855a9c06cc15d3e
SamuelQuetin <samquetinuel@gmail.com>
2025-02-24 08:40:21 +0100

    fix border sans v-sheet

--- src/components/Dialog/DialogLancerTraitement.vue
+++ src/components/Dialog/DialogLancerTraitement.vue
@@ -8,40 +8,36 @@
       width="500"
       type="table-heading, list-item-two-line, table-tfoot"
     >
-      <v-sheet class="pa-1" rounded color="primary" elevation="3">
-        <v-sheet class="pa-10" rounded>
-          <v-card class="ma-0 pa-6" elevation="0">
-            <v-card-title primary-title>{{title}}</v-card-title>
-            <v-card-text v-if="!isFinished">
-              <span style="font-size: small">
-                <p v-html="props.body"></p>
-              </span>
-            </v-card-text>
-            <v-card-text v-else>
-              <span style="font-size: small">
-                <p>
-                  Votre demande est en cours de traitement.<br/>
-                  Un mail vous informera du résultat.<br/>
-                  L'ensemble de vos demandes est à retrouver dans votre tableau de bord.<br/>
-                  <span v-if="rubrique">
-                    Rubrique : <b>{{ rubrique }}</b>
-                  </span>.
-                </p>
-                <p v-if="isDat" class="mt-4">
-                  <b>Pour une requête de type Date / Auteur / Titre, les demandes sont traitées chaque soir à 20h00</b>
-                </p>
-              </span>
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions class="justify-space-between">
-              <v-btn v-if="!isFinished" color="primary" @click="dialog = false" aria-label="Annuler">Annuler</v-btn>
-              <v-spacer v-else></v-spacer>
-              <v-btn v-if="!isFinished" color="primary" @click="confirm()" aria-label="Valider">Valider</v-btn>
-              <v-btn v-else color="primary" @click="router.push({name: route})" aria-label="OK">OK</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-sheet>
-      </v-sheet>
+      <v-card class="ma-0 pa-6 card-dialog-border" elevation="0">
+        <v-card-title primary-title>{{ title }}</v-card-title>
+        <v-card-text v-if="!isFinished">
+          <span style="font-size: small">
+            <p v-html="props.body"></p>
+          </span>
+        </v-card-text>
+        <v-card-text v-else>
+          <span style="font-size: small">
+            <p>
+              Votre demande est en cours de traitement.<br/>
+              Un mail vous informera du résultat.<br/>
+              L'ensemble de vos demandes est à retrouver dans votre tableau de bord.<br/>
+              <span v-if="rubrique">
+                Rubrique : <b>{{ rubrique }}</b>
+              </span>.
+            </p>
+            <p v-if="isDat" class="mt-4">
+              <b>Pour une requête de type Date / Auteur / Titre, les demandes sont traitées chaque soir à 20h00</b>
+            </p>
+          </span>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions class="justify-space-between">
+          <v-btn v-if="!isFinished" color="primary" @click="dialog = false" aria-label="Annuler">Annuler</v-btn>
+          <v-spacer v-else></v-spacer>
+          <v-btn v-if="!isFinished" color="primary" @click="confirm()" aria-label="Valider">Valider</v-btn>
+          <v-btn v-else color="primary" @click="router.push({name: route})" aria-label="OK">OK</v-btn>
+        </v-card-actions>
+      </v-card>
     </v-skeleton-loader>
   </v-dialog>
 </template>
@@ -51,7 +47,7 @@ import { ref } from 'vue';
 import router from '@/router';
 
 const dialog = defineModel();
-const emits = defineEmits(['launch'])
+const emits = defineEmits(['launch']);
 const props = defineProps({
   isLoading: {
     required: false,
@@ -80,13 +76,19 @@ const props = defineProps({
     required: false,
     type: Boolean
   }
-})
+});
 const title = ref(props.title);
 const isFinished = ref(false);
 
 function confirm() {
   emits('launch');
-  title.value = "Traitement validé";
+  title.value = 'Traitement validé';
   isFinished.value = true;
 }
 </script>
+<style>
+.card-dialog-border {
+  border: 2px solid rgb(var(--v-theme-primary));
+}
+
+</style>
